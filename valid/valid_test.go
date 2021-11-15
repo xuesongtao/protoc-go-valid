bfb157b6b104c20cfeccfa40a8ba1eac2ebb9c58
xuesongtao <xuesongtao512@qq.com>
2021-11-15 18:17:50 +0800

    add bench

--- valid/valid_test.go
+++ valid/valid_test.go
@@ -68,10 +68,10 @@ func TestTo(t *testing.T) {
 
 func TestOto(t *testing.T) {
 	type Tmp struct {
-		Name string `valid:"oto=1~3"`
-		Age  int32  `valid:"oto=0~100"`
+		Name     string `valid:"oto=1~3"`
+		Age      int32  `valid:"oto=0~100"`
 		NickName string `valid:"gt=1"`
-		Addr string `valid:"lt=3"`
+		Addr     string `valid:"lt=3"`
 	}
 	v := &Tmp{Name: "测试", Age: 0, NickName: "h1", Addr: "tets"}
 	t.Log(ValidateStruct(v))
@@ -79,7 +79,7 @@ func TestOto(t *testing.T) {
 
 func TestDate(t *testing.T) {
 	type Tmp struct {
-		Date string `valid:"date"`
+		Date     string `valid:"date"`
 		Datetime string `valid:"datetime"`
 	}
 	v := &Tmp{}
@@ -164,4 +164,114 @@ func TestProtoPb2(t *testing.T) {
 		Age:  0,
 	}
 	t.Log(ValidateStruct(m))
+}
+
+func BenchmarkValid(b *testing.B) {
+	testOrderDetailPtr := &TestOrderDetailPtr{
+		TmpTest3:  &TmpTest3{Name: "测试"},
+		GoodsName: "玻尿酸",
+	}
+	// testOrderDetailPtr = nil
+
+	testOrderDetails := []*TestOrderDetailSlice{
+		{TmpTest3: &TmpTest3{Name: "测试1"}, BuyerNames: []string{"test1", "hello2"}},
+		{TmpTest3: &TmpTest3{Name: "测试2"}, GoodsName: "隆鼻"},
+		{GoodsName: "丰胸"},
+		{TmpTest3: &TmpTest3{Name: "测试4"}, GoodsName: "隆鼻"},
+	}
+	// testOrderDetails = nil
+
+	u := &TestOrder{
+		AppName:              "集美测试",
+		TotalFeeFloat:        2,
+		TestOrderDetailPtr:   testOrderDetailPtr,
+		TestOrderDetailSlice: testOrderDetails,
+	}
+	for i := 0; i < b.N; i++ {
+		_ = ValidateStruct(&u, "alipay")
+	}
+}
+
+func BenchmarkIfValid(b *testing.B) {
+	testOrderDetailPtr := &TestOrderDetailPtr{
+		TmpTest3:  &TmpTest3{Name: "测试"},
+		GoodsName: "玻尿酸",
+	}
+	// testOrderDetailPtr = nil
+
+	testOrderDetails := []*TestOrderDetailSlice{
+		{TmpTest3: &TmpTest3{Name: "测试1"}, BuyerNames: []string{"test1", "hello2"}},
+		{TmpTest3: &TmpTest3{Name: "测试2"}, GoodsName: "隆鼻"},
+		{GoodsName: "丰胸"},
+		{TmpTest3: &TmpTest3{Name: "测试4"}, GoodsName: "隆鼻"},
+	}
+	// testOrderDetails = nil
+
+	u := &TestOrder{
+		AppName:              "集美测试",
+		TotalFeeFloat:        2,
+		TestOrderDetailPtr:   testOrderDetailPtr,
+		TestOrderDetailSlice: testOrderDetails,
+	}
+
+	vFn := func(info *TestOrder) {
+		// 说明: 写入的内容为随意写
+		errBuf := new(strings.Builder)
+		if len(info.AppName) >= 2 {
+			errBuf.WriteString("info.AppName len is short \n")
+		}
+
+		if len(info.AppName) <= 10 {
+			errBuf.WriteString("info.AppName len is long \n")
+		}
+
+		if info.TotalFeeFloat >= 2 {
+			errBuf.WriteString("info.TotalFeeFloat should more than 2 \n")
+		}
+
+		if info.TotalFeeFloat <= 5 {
+			errBuf.WriteString("info.TotalFeeFloat should more than 2 \n")
+		}
+
+		if info.TestOrderDetailPtr.TmpTest3 == nil {
+			errBuf.WriteString("info.TestOrderDetailPtr.TmpTest3 is nil \n")
+		} else {
+			if info.TestOrderDetailPtr.TmpTest3.Name == "" {
+				errBuf.WriteString("info.TestOrderDetailPtr.TmpTest3.Name is null \n")
+			}
+		}
+
+		if len(info.TestOrderDetailPtr.GoodsName) >= 2 {
+			errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+		}
+
+		if len(info.TestOrderDetailPtr.GoodsName) <= 5 {
+			errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+		}
+
+		if info.TestOrderDetailSlice == nil {
+			errBuf.WriteString("info.TestOrderDetailSlice is null \n")
+		} else {
+			for _, v := range info.TestOrderDetailSlice {
+				if v.TmpTest3 == nil {
+					errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+				} else {
+					if v.TmpTest3.Name == "" {
+						errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+					}
+				}
+
+				if len(v.BuyerNames) == 0 {
+					errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+				}
+
+				if v.GoodsName == "" {
+					errBuf.WriteString("info.TestOrderDetailPtr.GoodsName is null \n")
+				}
+			}
+		}
+	}
+	for i := 0; i < b.N; i++ {
+		vFn(u)
+	}
 }
