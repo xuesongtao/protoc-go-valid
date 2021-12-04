736d5ec8e21ffc737a767b2958759de816de5666
xuesongtao <xuesongtao512@qq.com>
2021-12-04 09:46:23 +0800

    update

--- build_tool.sh
+++ build_tool.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# 编译Windows
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o=protoc-go-valid-windows main.go
+# 根据不同平台进行编译
+function goBuild() {
+    echo "请输入要编译的平台: 1-Windows 2-Linux 3-Darwin"
+    read platform
+    case $platform in
+    1)
+        GOOS=windows
+        ;;
+    2)
+        GOOS=linux
+        ;;
+    3)
+        GOOS=darwin
+        ;;
+    *)
+        echo "输入不正确"
+        exit 1
+        ;;
+    esac
+    CGO_ENABLED=0 GOARCH=amd64 go build -o=protoc-go-valid main.go
+}
 
-# 编译linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o=protoc-go-valid-linux main.go
-
-# 编译darwin
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o=protoc-go-valid-darwin main.go
+goBuild
